61a794f0c50928c92bd084f3c7f7d34df88912ce
9bin08 <9bin08@naver.com>
2020-11-06 14:30:43 +0900

    Done

--- todo.js
+++ todo.js
@@ -13,36 +13,50 @@ function local() {
 function paintTodo(text) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-
+  const button = document.createElement("button");
+  const newId = toDos.length + 1;
+  li.id = newId;
+  button.addEventListener("click", delToDo);
   span.innerHTML = text;
-
+  button.innerHTML = "Del";
   toDoList.appendChild(li);
   li.appendChild(span);
+  li.appendChild(button);
 
   const toDoObj = {
     text,
-    id: toDos.length + 1,
+    id: newId,
   };
   toDos.push(toDoObj);
-  console.log(toDos);
+  local();
 }
 
 function handleSubmit(e) {
   event.preventDefault();
   const toDoWrite = toDoText.value;
   paintTodo(toDoWrite);
-  local(toDos);
+
   toDoText.value = "";
 }
 
 function getLoadList() {
-  const getList = JSON.parse(localStorage.getItem(toDoslocal));
-  if (getList === null) {
-  } else {
-    getList.forEach(function (toDos) {
+  const getList = localStorage.getItem(toDoslocal);
+  if (getList !== null) {
+    const parseTodo = JSON.parse(getList);
+    parseTodo.forEach((toDos) => {
       paintTodo(toDos.text);
     });
   }
+}
+function delToDo(e) {
+  const btn = e.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+
+  const removeDoto = toDos.filter((toDos) => {
+    return toDos.id !== parseInt(li.id);
+  });
+  toDos = removeDoto;
 }
 
 function init() {
